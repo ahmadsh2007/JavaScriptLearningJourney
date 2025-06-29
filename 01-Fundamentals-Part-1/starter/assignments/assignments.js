9789d3fe64efea622e32493f3add3785619b3ee3
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-06-29 19:53:24 +0300

    My first hour of the course

---
+++ 01-Fundamentals-Part-1/starter/assignments/assignments.js
@@ -0,0 +1,7 @@
+let country = "Jordan";
+let continent = "Asia";
+let population = 15;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+let js = "Amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Ahmad");
+console.log(23);
+
+let firstName = "Ahmad";
+console.log(firstName);
+
+// We should not start with numbers, and only allowed to use numbers, letters, _, and $
+// We use UPPER CASE for variables that never change like PI (check line 19 & 20)
+// Variables names should be understandable (check line 14-17)
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+let PI = 3.14159;
+console.log("the value of pi is", PI)
+
+console.log(myFirstJob);
