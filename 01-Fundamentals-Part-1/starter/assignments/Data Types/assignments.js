f8b13d2a35c8a10e1fa7dd7b3e180bbfcb1b17fd
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-07-06 21:28:52 +0300

    Data Type assignment finished

---
+++ 01-Fundamentals-Part-1/starter/assignments/Data Types/assignments.js
@@ -0,0 +1,12 @@
+// From the old assignment
+let country = "Jordan";
+let population = 15;
+
+// The new ones
+let isIsland = false;
+let language;
+
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof isIsland);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/assignments/README.md
@@ -0,0 +1,1 @@
+# [Jonas Assignments](https://jonas.io/assignments/instructions.html)
