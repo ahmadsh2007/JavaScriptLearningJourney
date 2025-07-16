c9944bd084fe3a5fcb2662073ed5cf06c6230241
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-07-16 13:15:40 +0300

    37. Functions Calling Other Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,6 +34,7 @@ console.log(appleJuice);
 */
 
 
+/*
 // * Funtion declaration
 const age1 = calcAge1(2007)
 
@@ -65,3 +66,17 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearsUntilRetirement(2007, 'Ahmad Shatnawi'));
+*/
+
+function cutFruit(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruit(apples);
+    const orangePieces = cutFruit(oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
