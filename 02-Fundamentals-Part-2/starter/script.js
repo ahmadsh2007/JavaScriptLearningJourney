befdebd3eb4197c20080f691d9f011e56a08cb6b
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-07-16 12:35:11 +0300

    34. Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,3 +10,23 @@ if (hasDriversLicense) console.log('I can drive');
 // const interface = 'Audi';
 // const private = 69;
 // const if = 2007;
+*/
+
+
+function logger() {
+    console.log("My name is Ahmad");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
