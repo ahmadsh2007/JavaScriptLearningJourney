c2eba6116aa8ccd0c229eaf0f05d5b70694d8be7
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-07-16 12:05:08 +0300

    33. Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audi';
+// const private = 69;
+// const if = 2007;
