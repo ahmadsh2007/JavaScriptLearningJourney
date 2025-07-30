bb966e66040a8b3844613c2007472dfec10ce82e
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-07-30 21:33:49 +0300

    75. What's the DOM and DOM Manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+/*
+* DOM (Document Object Model): structured representation of HTML documents. Allows JavaScript to access HTML Elements and Styles to manipulate them.
+
+* API stands for Application Programming Interface
+*/
