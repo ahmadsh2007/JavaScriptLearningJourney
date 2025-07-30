4a13e29603496dfd115f714aa986df4f99a75c0c
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-07-30 21:43:14 +0300

    76. Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,21 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
 /*
 * DOM (Document Object Model): structured representation of HTML documents. Allows JavaScript to access HTML Elements and Styles to manipulate them.
 
 * API stands for Application Programming Interface
 */
+
+console.log(document.querySelector('.message').textContent);
+// Change the text content
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 7;
+document.querySelector('.score').textContent = 13;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 13;
+console.log(document.querySelector('.guess').value);
